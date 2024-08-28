9de910728de5abee46e9b438c0eb7d86e1f8c938
Maxwell Furaha <maxwellfuraha91@gmail.com>
2024-08-28 13:59:50 +0300

    OOP Python

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 class Dog:
-    # Class body goes here
+    def sit(self):
+        print("The dog is sitting.")
+    def bark(self):
+        print("Woof!")
 
-    #Instance method definition
-    pass
+alj= Dog()
+alj.sit()
+alj.bark()
+
